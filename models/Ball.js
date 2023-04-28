d99a1705bed3700d2549b1e191e55e1163ab7241
sailajalakkakula <123012535+sailajalakkakula@users.noreply.github.com>
2023-04-27 20:54:15 -0500

    Update Ball.js

--- models/Ball.js
+++ models/Ball.js
@@ -13,21 +13,3 @@ const BallSchema = mongoose.Schema({
 
 module.exports = mongoose.model("Ball", BallSchema)
 
-// const mongoose = require("mongoose")
-
-// const BallSchema = mongoose.Schema({
-//   Ball_Type: String,
-//   Ball_Weight: {
-//     type: Number,
-//     min: [1, 'Weight should be at least 1'],
-//     max: [9, 'Weight should not exceed 10']
-//   },
-//   Ball_Cost: Number,
-//   Ball_Value: {
-//     type: Number,
-//     min: 124,
-//     max: 400
-//   }
-// })
-
-// module.exports = mongoose.model("Ball", BallSchema)
